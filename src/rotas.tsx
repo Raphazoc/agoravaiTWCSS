1696deea30f741bd785b7206eed7c871df0df2d8
Raphazoc <martinsraphael000@gmail.com>
2025-05-04 00:25:15 -0300

    atualizando o projeto usando TWCSS

---
+++ src/rotas.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import{ Layout } from './assets/layout/Layout';
+
+const rotas = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: (
+           <Layout>
+              H1
+          </Layout>
+           
+        ),
+      },   
+    ],
+  },
+]);
+
+export default rotas;
